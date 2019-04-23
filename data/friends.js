6e6b7b68b4f121a5eeaa27d3b01086001dbf0ceb
David Vo <davidvo1990@gmail.com>
2019-04-23 18:40:11 -0500

    dv

--- data/friends.js
+++ data/friends.js
@@ -7,21 +7,21 @@
 
 var friendArray = [
     {
-        name: "Ahmed",
-        photo: "https://placekitten.com/640/360",
-        scores: [
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5",
-            "5"
-        ]
-    }
+  "name":"Ahmed",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+}
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
